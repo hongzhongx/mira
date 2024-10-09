a0c8e464e7f9211ea793df9ff2e09363c29dffe9
hongzhongx <175797330+hongzhongx@users.noreply.github.com>
2024-10-09 14:11:27 +0800

    init code

---
+++ mira.cpp
@@ -0,0 +1,7 @@
+#include <multi_index_container_fwd.hpp>
+/*
+namespace mira { namespace multi_index {
+
+   void to_variant( const boost::tuples::null_type, fc::variant& vo ) {}
+   void from_variant( const fc::variant& vo, boost::tuples::null_type ) {}
+} }*/
